0ec2dcb9d79705502eba13bab22c9234a5a55ed0
Rayanne Dutra <rayanneoliveiradutra@gmail.com>
2025-01-03 13:14:40 -0300

    realizado ajustes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { LoginComponent } from './views/login/login.component';
 import {AgendamentosComponent} from './views/agendamentos/agendamentos.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' }, // Rota inicial redirecionando para Login
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'agendamentos', component: AgendamentosComponent },
 ];
