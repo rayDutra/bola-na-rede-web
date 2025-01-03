7eac160a00de1e76c09420118a83a37958c8ce58
Rayanne Dutra <rayanneoliveiradutra@gmail.com>
2025-01-03 13:10:04 -0300

    realizado ajustes de validação e design

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { ApiService } from '../../services/api.service';
-import {Router} from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
@@ -16,33 +17,39 @@ export class LoginComponent {
     password: ''
   };
 
+  loginErrorMessage: string = '';
+  passwordVisible: boolean = false;
+
   constructor(private apiService: ApiService, private router: Router) {}
 
-  onSubmit(form: NgForm): void {
-    if (form.valid) {
-      this.apiService.validateUser(this.user.email, this.user.password).subscribe({
-        next: (users) => {
-          if (users.length > 0 && users[0].password === this.user.password) {
-            alert('Login realizado com sucesso!');
-            console.log('Usuário logado:', users[0]);
-            console.log('Redirecionando para Agendamentos...');
-            this.router.navigate(['agendamentos']); // Redirecionamento
-          } else {
-            alert('E-mail ou senha incorretos.');
-          }
-        },
-        error: (err) => {
-          console.error('Erro ao validar usuário:', err);
-          alert('Erro ao tentar realizar o login. Tente novamente.');
-        }
-      });
-    } else {
-      alert('Por favor, preencha todos os campos corretamente.');
-    }
+  resetErrorMessage(): void {
+    this.loginErrorMessage = '';
   }
 
+  togglePasswordVisibility(): void {
+    this.passwordVisible = !this.passwordVisible;
+  }
 
-  navegarAgendamentos(ev:any){
-    this.router.navigate(['agendamentos']);
+  onSubmit(form: NgForm): void {
+    this.resetErrorMessage();
+
+    if (form.invalid) {
+      return;
+    }
+
+    this.apiService.validateUser(this.user.email, this.user.password).subscribe({
+      next: (users) => {
+        if (users.length > 0 && users[0].password === this.user.password) {
+          console.log('Usuário logado:', users[0]);
+          this.router.navigate(['agendamentos']);
+        } else {
+          this.loginErrorMessage = 'E-mail ou senha incorretos.';
+        }
+      },
+      error: (err) => {
+        console.error('Erro ao validar usuário:', err);
+        this.loginErrorMessage = 'Erro ao processar a solicitação. Tente novamente mais tarde.';
+      }
+    });
   }
 }
